61bb4200181ef8b39184afda897f6834eb0321b2
Elya Megrabyan <m-elia@yandex.ru>
2023-03-10 23:36:48 +0300

    Первая часть: ввод массива

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,1 +1,19 @@
 ﻿// Итоговый проект
+
+
+Console.Clear();
+Console.Write("Введите длину массива");   // ввод длины массива
+int n = int.Parse(Console.ReadLine());
+
+string[] array = inputarray(n);  // Вызов метода создания и ввода нового массива
+
+string[] inputarray(int n)   // Метод создания и ввода нового массива
+{
+    string[] arr= new string[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine($"Введите значение массива {i}");
+        arr[i] = Console.ReadLine();
+    }
+return arr;    
+}
